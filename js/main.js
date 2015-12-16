47daad7294cc6a4200c3676daf206c02b5ded022
Kristen Zirkler <bazirkler@gmail.com>
2015-12-16 15:46:16 -0700

    Design In progress

--- js/main.js
+++ js/main.js
@@ -7,4 +7,22 @@ $(function(){
 		$('.nav').toggleClass('nav-open');
 	});
 
+	// Sticky Photo
+	function navFade(){
+		var y = $(document).scrollTop();
+		var t = $('.content').offset().top;
+		//var t = 70;
+
+		if (y > t) {
+		    $('.site-navigation').addClass('sticky');
+		} else {
+		    $('.site-navigation').removeClass('sticky');
+		}
+	}
+	// Scroll Functions
+	$(document).scroll(function () {
+		navFade();
+	});
+
+
 });
